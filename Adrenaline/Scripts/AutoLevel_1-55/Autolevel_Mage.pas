65b196b4c37ef73242d0f5612cee391ce3f44eb2
ICCT | NOTE <it.tshp3@slmail.me>
2023-12-06 12:21:31 -0400

    teste

---
+++ Adrenaline/Scripts/AutoLevel_1-55/Autolevel_Mage.pas
@@ -0,0 +1,80 @@
+uses SysUtils,TBuff,TTeleport,TPath;
+
+var Figth_Flag:boolean;
+     Item: TL2Item;
+     Npc: TL2Npc;
+     Obj: TL2Buff;
+     IdBuff: integer;
+     Buffs:TL2Buff;
+var L2Skill: TL2Skill;
+
+procedure toVillageIfDeath;
+begin
+  TUtilitarios.FtoVillageIfDeath;
+end;
+
+//Macro Buff | Fighter
+procedure toBuff;
+begin
+   TBuff.FFighter;
+   delay(500);
+   toSpot;
+end;
+
+procedure toTownzone;
+begin
+    TTeleport.FTownzone;
+    delay(500);
+    toBuff;
+end;
+
+procedure toRebuff;
+  begin
+  IdBuff:= 1323 ; //Check BUFF
+  toVillageIfDeath;
+   while not User.Buffs.ById(IdBuff,obj) do begin
+        Print('Sem Nobles');
+        toVillageIfDeath;
+        Engine.Facecontrol(0,False);
+        toTownzone;
+   end;
+end;
+
+procedure toSpot;
+begin
+//First spot 5-20
+if ((User.Level>4) and (User.Level<20)) then begin
+begin
+    TTeleport.FFirst;
+    delay(500);
+    TPath.FFirst;
+end;
+
+//Teleport to second spot 20-40
+if ((User.Level>19) and (User.Level<40)) then begin
+begin
+    TTeleport.FSecond;
+    delay(500);
+    TPath.FSecond;
+end;
+
+//Teleport to third spot 40-55
+if ((User.Level>39) and (User.Level<55)) then begin
+procedure TeleportThirdSpot;
+begin
+    TTeleport.FThird;
+    delay(500);
+    TPath.FThird;
+end;
+
+end;
+
+//-----------------------------------------------------------------------------
+begin    //repete novamente o script
+Print('repete novamente');
+  repeat
+   toVillageIfDeath;
+   toBuff;
+  until Engine.Status = lsOffline;
+  Delay(5000);
+end.
---
+++ Adrenaline/Scripts/AutoLevel_1-55/TPath.pas
@@ -0,0 +1,44 @@
+unit TPath;
+
+interface
+
+implementation
+
+procedure FFirst;
+   begin
+   Print('Fight zone');
+    Engine.MoveTo(26936, 247352, -3216);
+    Print('Primeira Zona');
+    Delay(1000);
+    Engine.MoveTo(27544, 246680, -3648);
+    Print('Segunda Zona');
+    TLoadingClass.FBSFarmAnomic2;    //COLOCA NOME DO CHAR AQUI
+    Print('Start Farm!');
+    Engine.Facecontrol(0,True);
+end;
+
+procedure FSecond;
+   begin
+   Print('Fight zone');
+    Engine.MoveTo(26936, 247352, -3216);
+    Print('Primeira Zona');
+    Delay(1000);
+    Engine.MoveTo(27544, 246680, -3648);
+    Print('Segunda Zona');
+    TLoadingClass.FBSFarmAnomic2;    //COLOCA NOME DO CHAR AQUI
+    Print('Start Farm!');
+    Engine.Facecontrol(0,True);
+end;
+
+procedure FThird;
+   begin
+   Print('Fight zone');
+    Engine.MoveTo(26936, 247352, -3216);
+    Print('Primeira Zona');
+    Delay(1000);
+    Engine.MoveTo(27544, 246680, -3648);
+    Print('Segunda Zona');
+    TLoadingClass.FBSFarmAnomic2;    //COLOCA NOME DO CHAR AQUI
+    Print('Start Farm!');
+    Engine.Facecontrol(0,True);
+end;
