52178c8a44c336da83750adf397e2fd15aac4e4f
GFM-Network <it.tshp3@slmail.me>
2023-12-08 13:27:29 -0400

    update

--- Adrenaline/Scripts/AutoLevel_Mage_1-55/Autolevel_Mage.pas
+++ Adrenaline/Scripts/AutoLevel_Mage_1-55/Autolevel_Mage.pas
@@ -1,89 +1,91 @@
-uses SysUtils,TBuff,TTeleport,TPath;
+uses SysUtils, TBuff, TTeleport, TPath;
 
-var Figth_Flag:boolean;
-     Item: TL2Item;
-     Npc: TL2Npc;
-     Obj: TL2Buff;
-     IdBuff: integer;
-     Buffs:TL2Buff;
-var L2Skill: TL2Skill;
+var
+  Figth_Flag: boolean;
+  Item: TL2Item;
+  Npc: TL2Npc;
+  Obj: TL2Buff;
+  IdBuff: integer;
+  Buffs: TL2Buff;
+var
+  L2Skill: TL2Skill;
 
-//Dead Character goes to village
+// Dead Character goes to village
 procedure toVillageIfDeath;
 begin
   TTools.FtoVillageIfDeath;
 end;
 
-//Macro Buff | Fighter
+// Macro Buff | Fighter
 procedure toBuff;
 begin
-   TBuff.FMage;
-   delay(500);
-   toSpot;
+  TBuff.FMage;
+  Delay(500);
+  toSpot;
 end;
 
-//Return to Town
+// Return to Town
 procedure toTownzone;
 begin
-    TTeleport.FTownzone;
-    delay(500);
-    toBuff;
+  TTeleport.FTownzone;
+  Delay(500);
+  toBuff;
 end;
 
-//Rebuff Noblesse Blessing
+// Rebuff Noblesse Blessing
 procedure toRebuff;
 begin
-  IdBuff:= 1323 ; //Check BUFF
+  IdBuff := 1323; // Check BUFF
   toVillageIfDeath;
-while not User.Buffs.ById(IdBuff,obj) do begin
-        Print('Without Noblesse Blessing');
-        toVillageIfDeath;
-        Engine.Facecontrol(0,False);
-        toTownzone;
-   end;
+  while not User.Buffs.ById(IdBuff, obj) do
+  begin
+    Print('Without Noblesse Blessing');
+    toVillageIfDeath;
+    Engine.Facecontrol(0, False);
+    toTownzone;
+  end;
 end;
+
 
 //Teleport to spot based on level
 procedure toSpot;
 begin
-
-//Kill Gremlins to reach lv 5
-if (User.Level>5) then begin
-begin
-    Engine.SetTarget (Gremlin);
-    Delay (1000)
-    Engine.LoadConfig('');    //CHARACTER NAME
-    Engine.LoadZone('');   //ZMAP NAME
+  // Kill Gremlins to reach level 5
+  if (User.Level < 5) then
+  begin
+    Engine.SetTarget(Gremlin);
+    Delay(1000);
+    Engine.LoadConfig('');    // CHARACTER NAME
+    Engine.LoadZone('');      // ZMAP NAME
     Print('DONE!');
     toVillage;
-    Engine.Facecontrol(0,True);
-end;
-
-//First spot lv 5-20
-if ((User.Level>4) and (User.Level<20)) then begin
-begin
-    TTeleport.FFirst;
-    delay(500);
-    TPath.FFirst;
-end;
-
-//Teleport to second spot lv 20-40
-if ((User.Level>19) and (User.Level<40)) then begin
-begin
-    TTeleport.FSecond;
-    delay(500);
-    TPath.FSecond;
-end;
-
-//Teleport to third spot lv 40-55
-if ((User.Level>39) and (User.Level<55)) then begin
-procedure TeleportThirdSpot;
-begin
-    TTeleport.FThird;
-    delay(500);
-    TPath.FThird;
-end;
-
+    Engine.Facecontrol(0, True);
+  end
+  else
+  begin
+    // First spot level 5-20
+    if ((User.Level >= 5) and (User.Level < 20)) then
+    begin
+      TTeleport.FFirst;
+      Delay(500);
+      TPath.FFirst;
+    end
+    // Teleport to second spot level 20-40
+    else if ((User.Level >= 20) and (User.Level < 40)) then
+    begin
+      TTeleport.FSecond;
+      Delay(500);
+      TPath.FSecond;
+    end
+    // Teleport to third spot level 40-55
+    else if ((User.Level >= 40) and (User.Level < 55)) then
+    begin
+      TTeleport.FThird;
+      Delay(500);
+      TPath.FThird;
+    end;
+    // Add more spots as needed
+  end;
 end;
 
 //-----------------------------------------------------------------------------
