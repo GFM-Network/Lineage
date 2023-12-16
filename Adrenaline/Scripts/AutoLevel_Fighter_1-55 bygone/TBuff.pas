a80b737e5d0b76fc272ee14adf185adbe76582e7
GFM-Network <it.tshp3@slmail.me>
2023-12-16 00:14:36 -0400

    .

---
+++ Adrenaline/Scripts/AutoLevel_Fighter_1-55 bygone/TBuff.pas
@@ -0,0 +1,29 @@
+unit TBuff;
+
+interface
+
+implementation
+
+procedure FFighter;
+begin
+  Engine.BypassToServer('_bbshome');
+  Delay(500);
+  Engine.BypassToServer('_bbsbuffer');
+  Delay(500);
+  Engine.BypassToServer('_bbsbufferbypass_giveBuffSet figher 0 0');
+  Delay(500);
+  Print('Character is buffed with fighter macro!');
+end;
+
+procedure FMage;
+begin
+ Engine.BypassToServer('_bbshome');
+  Delay(500);
+  Engine.BypassToServer('_bbsbuffer');
+  Delay(500);
+  Engine.BypassToServer('_bbsbufferbypass_giveBuffSet mage 0 0');
+  Delay(500);
+  Print('Character is buffed with mage macro!');
+end;
+
+end.
